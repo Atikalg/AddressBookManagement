a78d8750c9ea24303818d952d32b5b25f6221004
atikalg <who.atik@gmail.com>
2022-04-16 20:10:24 +0530

    Added : Welcome message in address book management

---
+++ src/main/java/com/blridgelab/addressBook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.blridgelab.addressBook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook Management System");
+    }
+}
