2d5d6bfdcf05bc801bcdbe417f157fc08c72d4dd
atikalg <who.atik@gmail.com>
2022-04-18 15:37:34 +0530

    Added : Ability to create contacts in address book

---
+++ src/main/java/com/blridgelab/addressBook/Address.java
@@ -0,0 +1,41 @@
+package com.blridgelab.addressBook;
+
+import java.util.Scanner;
+
+public class Address {
+    Contact contact;
+    Scanner sc = new Scanner(System.in);
+
+    public void addContact() {
+        contact = new Contact();
+        System.out.println("Enter the first name : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter the last name : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter the phone no : ");
+        String phoneNumber = sc.nextLine();
+        System.out.println("Enter the city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter the state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter the pincode : ");
+        String pincode = sc.nextLine();
+        System.out.println("Enter Email id : ");
+        String email = sc.nextLine();
+
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        contact.setPhoneNumber(phoneNumber);
+        contact.setCity(city);
+        contact.setState(state);
+        contact.setPincode(pincode);
+        contact.setEmail(email);
+        System.out.println(contact);
+
+    }
+
+    public void display() {
+        String str = contact.getFirstName();
+        System.out.println("str = " + str);
+    }
+}
--- src/main/java/com/blridgelab/addressBook/AddressBookMain.java
+++ src/main/java/com/blridgelab/addressBook/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.blridgelab.addressBook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Management System");
+
     }
 }
