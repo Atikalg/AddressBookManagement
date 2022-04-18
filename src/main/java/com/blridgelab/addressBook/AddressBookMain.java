12f8467f41f4171d34495105b8703ec2d7a69011
atikalg <who.atik@gmail.com>
2022-04-18 15:42:50 +0530

    Added : Ability to add contacts in address book

--- src/main/java/com/blridgelab/addressBook/AddressBookMain.java
+++ src/main/java/com/blridgelab/addressBook/AddressBookMain.java
@@ -3,6 +3,8 @@ package com.blridgelab.addressBook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Management System");
-
+        Address address = new Address();
+        address.addContact();
+        address.display();
     }
 }
