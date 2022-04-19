38320ba13e9920d3cdd17db2ecbdacbdd8adddf0
atikalg <who.atik@gmail.com>
2022-04-19 16:46:32 +0530

    Added : Updated Main function for multiple contact

--- src/main/java/com/blridgelab/addressBook/AddressBookMain.java
+++ src/main/java/com/blridgelab/addressBook/AddressBookMain.java
@@ -8,23 +8,6 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Management System");
         Address address = new Address();
-        address.addContact();
-        address.display();
-
-        System.out.println("Do you want edit or delete  contacts ? ");
-        System.out.println(" Enter E for edit ");
-        System.out.println(" Enter D for delete");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        if (input.equals("e") || input.equals("E")) {
-            System.out.println("Enter Following data : ");
-            address.editContact();
-        }
-        else if (input.equals("d") || input.equals("D")) {
-            address.deleteContact();
-        }
-        else {
-            System.out.println(" Thank you ");
-        }
+        address.getmenu();
     }
 }
