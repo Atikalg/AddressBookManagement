562460c2198182ab6ea460716588765735182a3e
atikalg <who.atik@gmail.com>
2022-05-22 21:58:58 +0530

    Added : Ability to write address book in JSON file

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,8 +1,10 @@
 package com.bridgelab.AddressBook;
 /*
  * @author: Atik
- * Ability to read and write address in CSV file
+ * Ability to read and write address in JSON file
  */
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,7 +16,7 @@ public class AddressBookMain {
     static Scanner sc = new Scanner(System.in);
     static Map<String, AddressBook> addressBookDirectory = new HashMap<String, AddressBook>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("-----------------Welcome to address book Program --------------------");
         System.out.println();
         System.out.println("Operation successful.");
@@ -72,7 +74,7 @@ public class AddressBookMain {
     /*
      * Adding new address book to by checking existing book is available or not
      */
-    public static void addAddressBook() throws IOException {
+    public static void addAddressBook() throws IOException, ParseException {
         AddressBook addressBook = new AddressBook();
         System.out.println("Enter the name of the Address Book you want to add");
         String bookNameToAdd = sc.next();
@@ -89,9 +91,9 @@ public class AddressBookMain {
     }
 
     /*
-     * in this method.. calling existing address book and editing them.
+     * in this method calling existing address book and editing them.
      */
-    public static void editAddressBook() throws IOException {
+    public static void editAddressBook() throws IOException, ParseException {
 
         System.out.println("Enter the Name of the Address Book which you want to edit:");
         String addressBookToEdit = sc.next();
